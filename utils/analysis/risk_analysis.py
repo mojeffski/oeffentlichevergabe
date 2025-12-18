265e2b0a36569414bbdfdfad92f433289d80827c
Moritz Neujeffski <m.neujeffski@posteo.de>
2025-12-18 22:35:11 +0100

    improved test_scriped; added analysis folder in utils for risk_analysis files

---
+++ utils/analysis/risk_analysis.py
@@ -0,0 +1,9 @@
+#contains functions for risk analysis that contain the logic to analyze tenders for risk indicators
+
+
+def analyze_risk(tender):
+    # Placeholder for a function that provides a risk analysis logic
+    print('testing imports')
+    #pass
+    
+    
